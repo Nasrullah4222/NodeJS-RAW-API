da2a84680cd7245442862f133b896c0fb20a5843
NASRULLAH KAISHER SIJAN <nasrullah4222@gmail.com>
2026-05-13 19:56:08 +0600

    CRUD Operation Added.

--- index.js
+++ index.js
@@ -8,14 +8,34 @@
 // Dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./handler/routeHandeler/environment');
+const data = require('./lib/data');
 
 // app object-module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-    port: 3000,
-};
+//testing file system
+//ToDO: Pore Dikha jabe
+
+data.create('test', 'newFile', { name: 'Bangladesh', language: 'Bangla' }, err => {
+    console.log(err);
+});
+
+/*
+data.read('test', 'newFile', (err, data) => {
+    console.log(err, data);
+});
+
+data.update('test', 'newFile', { name: 'England', language: 'English' }, err => {
+    console.log(err);
+});
+
+data.delete('test', 'newFile', err => {
+    if (!err) {
+        console.log('File deleted successfully');   
+    }
+});
+*/
 
 // Handle Request Responses
 app.handleReqRes = handleReqRes;
@@ -23,9 +43,8 @@ app.handleReqRes = handleReqRes;
 // create server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`envirom=nment variable is ${process.env.NODE_ENV}`);
-        console.log(`listening on port ${app.config.port}`);
+    server.listen(environment.port, () => {
+        console.log(`listening on port ${environment.port}`);
     });
 };
 
